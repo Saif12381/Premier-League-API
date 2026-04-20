736b5c08c83b993dff7cdbf57e88c43e30d106d5
saif <saif797879@gmail.com>
2026-04-20 09:38:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ let randomNumber = Math.floor(Math.random() * 10);
 
 
 fetch("https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php?l=English_Premier_League")
-.then(response => response.json())
-.then(function(data) {
-teamsList = data.teams;
-console.log(teamsList);
-button.addEventListener('click', function() {
-console.log("clicked");
-choice = dropdown.value;
-console.log("Selected choice:", choice);
-output.innerHTML = teamsList[randomNumber].strTeam;
+    .then(response => response.json())
+    .then(function (data) {
+        teamsList = data.teams;
+        console.log(teamsList);
+        button.addEventListener('click', function () {
+            randomNumber = Math.floor(Math.random() * 10);
+            console.log("clicked");
+            choice = dropdown.value;
+            console.log("Selected choice:", choice);
+            output.innerHTML = teamsList[randomNumber].strTeam;
 
-})});
+        })
+    });
 
 
